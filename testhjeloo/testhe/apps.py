1c7962fc5397291a6f7506a1825ef026fe875e87
topsoft2021 <topsoft2021@gmail.com>
2021-01-20 17:39:31 +0330

    test

---
+++ testhjeloo/testhe/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TestheConfig(AppConfig):
+    name = 'testhe'
---
+++ testhjeloo/testhjeloo/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testhjeloo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testhjeloo.settings')
+
+application = get_wsgi_application()
